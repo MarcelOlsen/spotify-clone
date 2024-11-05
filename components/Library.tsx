3abd33732528b341c59b039a482aead2f553a655
MarcelOlsen <marcelolsen26@gmail.com>
2024-11-05 20:52:30 +0100

    Created supabase upload functionality

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,12 +1,23 @@
 "use client";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
 import React from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 
 const Library = () => {
+  const authModal = useAuthModal();
+  const { user } = useUser();
+  const uploadModal = useUploadModal();
+
   const onClick = () => {
-    //handle upload later
+    if (!user) return authModal.onOpen();
+
+    //TODO: check for subscription
+
+    return uploadModal.onOpen();
   };
 
   return (
